63c03267daa2e75cbffc063ee100580656ca4d8b
Mayur Saroj <mayursarojofficial@gmail.com>
2022-07-09 10:43:13 +0530

    :lipstick: Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import DownloadSection from "./components/DownloadSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Header />
       <DownloadSection />
+      <Footer />
     </div>
   );
 }
