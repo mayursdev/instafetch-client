39f48c87d0585c308f44ec78a60d5b4ef56814d1
Mayur Saroj <mayursarojofficial@gmail.com>
2022-07-11 12:56:43 +0530

    :bug: Fix undefined in alt and title of postMedia
    Remove the postCode reference

--- src/components/DownloadSection/PostMedia.jsx
+++ src/components/DownloadSection/PostMedia.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const PostMedia = (props) => {
   const { postDetails } = props;
-  const postCode = postDetails.code;
   const base64ImgPreview = postDetails.base64EncodedPreview;
 
   return (
@@ -10,8 +9,8 @@ const PostMedia = (props) => {
       <img
         src={`data:image/jpg;base64, ${base64ImgPreview}`}
         className="sm:h-72 w-full sm:object-cover sm:hover:object-contain transition-all"
-        alt={`thumbnail https://instagram.com/p/${postCode}/`}
-        title={`thumbnail https://instagram.com/p/${postCode}/`}
+        alt={`post thumbnail`}
+        title={`post thumbnail`}
       />
     </div>
   );
